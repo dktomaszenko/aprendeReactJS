d32c34fea3b3ce527654375c05fd65672b1ce5a5
dktomaszenko <dktomaszenko@gmail.com>
2018-06-26 13:08:48 +0200

    props

--- src/App.js
+++ src/App.js
@@ -1,21 +1,48 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
+class Hello extends Component {
+    render() {
+        return <h1 className="App-title">{this.props.title}</h1>
+    }
+}
+
+class Text extends Component {
+render (){
+    const { isActivated , arrayOfNumbers, objectWithInfo, multiply} = this.props;
+    const textoSegunBool = isActivated ? 'On': 'Off';
+    const mappedNumber = arrayOfNumbers.map(multiply);
     return (
-      <div className="App"
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Bienvenido en React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <div>
+            <p className="App-intro">{mappedNumber.join(', ')}</p>
+            <p className="App-intro">{objectWithInfo.key}</p>
+        </div>
     )
-  }
+}
+}
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <Hello title='Welcome to React props' />
+                </header>
+                <Text
+                    arrayOfNumbers={[2,3,10]}
+                    objectWithInfo={{
+                        key: 'First',
+                        key2: 'otherValue'
+                    }}
+                    isActivated
+                    multiply={(number) => number * 4}
+                    number={2}
+                />
+            </div>
+        );
+    }
 }
 
 export default App;
