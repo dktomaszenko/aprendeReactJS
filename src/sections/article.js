ec91e1bd500acdb0a0b6df6c29bbb4d634091efd
darius <dktomaszenko@gmail.com>
2018-07-02 00:36:22 +0200

    componentDidMount

--- src/sections/article.js
+++ src/sections/article.js
@@ -5,14 +5,25 @@ import PropTypes from 'prop-types';
 export default class Article extends Component {
 
     static propTypes = {
-        title: PropTypes.string
+        title: PropTypes.string.isRequired
     };
 
     constructor(props){
         super(props);
+        console.log('constructor');
+    }
+
+    componentWillMount(){
+        console.log('componentWillMount');
+        /*debugger*/
+    }
+
+    componentDidMount(){
+        console.log('componentDidMount');
     }
 
     render(){
+        console.log('render');
         const {title, children} = this.props;
         return (
             <section>
